8901060ae7316b8035361a2c783fdf714b8cad65
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2025-10-27 23:32:35 +0600

    doc: add comment for UpdateBugDto

--- backend/src/bugs/dto/update-bug.dto.ts
+++ backend/src/bugs/dto/update-bug.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBugDto } from './create-bug.dto';
 
+// Create DTO for updating bugs with every property of creation DTO made optional
 export class UpdateBugDto extends PartialType(CreateBugDto) {}
